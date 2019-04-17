d974d94bb0d36d72fa54dbe9ab3f4ef89ea3e177
alejandro <alejandro.tang@utec.edu.pe>
2019-04-16 21:41:41 -0500

    Primer commit

---
+++ cmake-build-debug/CMakeFiles/typetraits-Mandarine2312.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/typetraits-Mandarine2312.dir/main.cpp.o"
+  "CMakeFiles/typetraits-Mandarine2312.dir/test/tester.cpp.o"
+  "CMakeFiles/typetraits-Mandarine2312.dir/mock/mocker.cpp.o"
+  "typetraits-Mandarine2312.pdb"
+  "typetraits-Mandarine2312"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/typetraits-Mandarine2312.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
